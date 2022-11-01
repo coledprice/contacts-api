15b26f3d5fb6b6aab31a0d68df267fad684cd7e1
Cole Price <colevart@gmail.com>
2022-11-01 16:53:46 -0400

    init

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,11 @@
 class ContactsController < ApplicationController
+  def all_contacts
+    contacts = Contact.all
+    render json: contacts.as_json
+  end
+
+  def one_contact
+    contact = Contact.last
+    render json: contact.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/all_contacts", controller: "contacts", action: "all_contacts"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "one_contact", controller: "contacts", action: "one_contact"
 end
