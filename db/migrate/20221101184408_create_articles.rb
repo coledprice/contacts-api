e92e0b8eb99303f1ed1f30be321167deb9283a09
Cole Price <colevart@gmail.com>
2022-11-01 16:24:05 -0400

    init

---
+++ db/migrate/20221101184408_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :phone_number
+
+      t.timestamps
+    end
+  end
+end
